4385b44d90b9c46616cddeb08fd8f794e1957f79
KayyoungHL <kayyounghl@gmail.com>
2021-11-21 15:55:10 +0900

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+from calculator.calculator import Calculator
+
+def __main():
+    print("main start")
+    x = Calculator()
+    print(x.start())
+    print("main end")
+
+if __name__=="__main__":
+    __main()
